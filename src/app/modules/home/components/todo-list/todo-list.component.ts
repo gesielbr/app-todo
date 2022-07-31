46ec22e2a1aa9389f10d28a0eae996f8a03836cc
Gesiel Oliveira <gesiel.oliveira@linx.com.br>
2022-07-31 20:42:17 -0300

    Delete itens

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova task 1', checked: true },
+    { task: 'Minha nova task 2', checked: true },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public deleteItemTaskList(event: any) {
+    this.taskList.splice(event, 1);
   }
 
+  public deleteAllTaskList() {
+    const confirm = window.confirm(
+      'Você realmente deseja apagar todos os itens da lista?'
+    );
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
